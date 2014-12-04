18d3272d6e4726a892f2b211b782f94a90d24ea3
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2014-12-04 16:21:14 +0530

    added role feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [:seller, :buyer, :admin]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :confirmable, :lockable
+
+  has_many :seller_bookings, class_name: "Booking", foreign_key: "seller_id"
+  has_many :buyer_bookings, class_name: "Booking", foreign_key: "buyer_id"
+         
 end
