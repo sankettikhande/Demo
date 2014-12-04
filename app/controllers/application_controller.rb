602bca341f97024a08d9cda57ab8d01c9764f4ec
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2014-12-04 18:02:23 +0530

    users edit feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     permitted_parameters = [:first_name, :last_name, :company_name, :company_registration_number, :address_line_1, :address_line_2, :address_line_3, :post_code, :city, :state, :country, :phone_number, :preferred_currency]
     devise_parameter_sanitizer.for(:sign_up) << permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << permitted_parameters
   end
 end
