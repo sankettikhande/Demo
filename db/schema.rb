e64a74da12d887ab4f7aa684e870ca2807ba33b8
KiranKumara <kiran.kumara@sodelsolutions.com>
2014-12-04 20:14:53 +0530

    Added carrierwave gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204130218) do
+ActiveRecord::Schema.define(version: 20141204143906) do
 
   create_table "bookings", force: true do |t|
     t.integer  "buyer_id"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20141204130218) do
     t.string   "phone_number"
     t.string   "preferred_currency"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
