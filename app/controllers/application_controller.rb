87829ce5ad6ea6e6554c64c0df94a9a8c714f180
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2014-12-04 12:11:17 +0530

    permitted  parameter for devise additional attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    permitted_parameters = [:first_name, :last_name, :company_name, :company_registration_number, :address_line_1, :address_line_2, :address_line_3, :post_code, :city, :state, :country, :phone_number, :preferred_currency]
+    devise_parameter_sanitizer.for(:sign_up) << permitted_parameters
+  end
 end
