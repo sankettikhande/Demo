205ec76dffaec075b21207c63d03cebcd361190c
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2014-12-05 18:23:30 +0530

    UI design integrated for Home page

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.aside-toggle').click(function(){
+		$(this).toggleClass('animate');
+		$('.side-wrapper').toggleClass('active');
+	});
+	$('#search-more-btn').click(function(){
+		$($('.search-list .search-row').first().html()).clone().insertAfter(".search-row");
+	});
+	$('#forgot-link').click(function(){
+		$('.login-form').toggleClass('forgot-form');
+	});
+	$('.datepicker,.input-group.date').datepicker();
+	$('[data-toggle="tooltip"]').tooltip();
+	$('#login-modal').modal('show')
+});
