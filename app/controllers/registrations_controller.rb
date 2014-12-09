42a7f9492fdf92fea78ad220cf3353948c484808
KiranKumara <kiran.kumara@sodelsolutions.com>
2014-12-09 19:23:09 +0530

    guest user form refactor

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def register_guest_user
-    email = params[:user][:email]
-    user_exists = User.exists?(email: email)
 
-    if user_exists
-      redirect_to get_quotes_freights_path, notice: "Email allready taken!"
+    if guest_user_params.values.any?{|v| v.blank?}
+      render :template => "freights/guest_user_sign_up", locals: { notice: "All fields are required" }
     else
+      email = params[:user][:email]
+      return render :template => "freights/guest_user_sign_up", locals: { notice: "Email allready taken!" } if User.exists?(email: email)
       user = User.new(guest_user_params.merge({role: :guest, password: email, password_confirmation: email}))
       user.skip_confirmation!
       user.save!
