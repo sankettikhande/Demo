f915e87f6f8cc428e01dea42913d4190c3ccc165
KiranKumara <kiran.kumara@sodelsolutions.com>
2014-12-09 20:33:25 +0530

    Added Csv import functionality for freight data.

--- app/models/freight.rb
+++ app/models/freight.rb
@@ -1,2 +1,12 @@
+require 'csv'
 class Freight < ActiveRecord::Base
+
+  def self.imports(file)
+    freights = []
+    CSV.foreach(file.path, headers: true) do |row|
+      row_hash = row.to_hash
+      freights << Freight.new(row_hash.merge(start_date: Date.parse(row_hash["start_date"]), end_date: Date.parse(row_hash["end_date"])))
+    end
+    Freight.import freights
+  end
 end
