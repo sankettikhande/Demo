72a5b927677b0e1e240f9bba5bcff50a60e674a2
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2014-12-10 13:31:50 +0530

    added session to fill search form with last search

--- app/controllers/freights_controller.rb
+++ app/controllers/freights_controller.rb
@@ -1,14 +1,11 @@
 class FreightsController < ApplicationController
 
-  layout 'application'
-
   def search
-    session[:search] = params[:search].first if params[:search].present?
-    search_parameter = session[:search]
-    @freight_rates = Freight.search conditions: {source: search_parameter[:source], destination: search_parameter[:destination]}
-    respond_to do |format |
+    session['search'] = params[:search].first if params[:search].present?
+    @freight_rates = Freight.search conditions: {source: session['search']['source'], destination: session['search']['destination']}
+    respond_to do |format|
       if current_user
-        id = Booking.create(buyer_id: current_user.id, source: search_parameter[:source], destination: search_parameter[:destination], aasm_state: 'draft').id
+        id = Booking.create(buyer_id: current_user.id, source: session['search']['source'], destination: session['search']['destination'], aasm_state: 'draft').id
         format.html { redirect_to get_quote_bookings_path(id:id)}
       else
           format.html { render :template => "freights/guest_user_sign_up"}
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
     
   def landing
+    session['search'] = {} if session['search'].nil?
+     
   end
   
 end
