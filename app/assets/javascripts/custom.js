78de96487aa7723119e3eded2839ae09aea54847
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2014-12-13 23:07:03 +0530

    added date picker via data html provider

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,28 +3,45 @@ $(document).ready(function(){
 		$(this).toggleClass('animate');
 		$('.side-wrapper').toggleClass('active');
 	});
-	$('#search-more-btn').click(function(){
+
+	$('body').on('click', '#search-more-btn', function(){
 		$($('.search-list .search-row').first().html()).clone().insertAfter(".search-row");
 	});
 
-    $('#forgot-link').click(function(){
-        $('.login-form').addClass('hidden');
-        $('.forgot-form').removeClass('hidden');
-    });
+  $('#forgot-link').click(function(){
+      $('.login-form').addClass('hidden');
+      $('.forgot-form').removeClass('hidden');
+  });
 
-    $('#login-link').click( function(){
-        $('#login-modal').modal('show');
-    });
+  $('#login-link').click( function(){
+      $('#login-modal').modal('show');
+  });
 
-    $('#login-modal').on("hidden.bs.modal", function(){
-        $('.forgot-form').addClass('hidden');
-        $('.login-form').removeClass('hidden');
-    });
+  $('#login-modal').on("hidden.bs.modal", function(){
+      $('.forgot-form').addClass('hidden');
+      $('.login-form').removeClass('hidden');
+  });
 
 	$('.selecter').selecter();
 
-    $('.date').datepicker({
-        format: 'yyyy/mm/dd'
-    });
+  // $('.date').datepicker({
+  //     format: 'yyyy/mm/dd'
+  // });
+
+  $('[data-toggle="tooltip"]').tooltip({
+     container: 'body'
+  });
+
+  $(function(){
+   $('body').on('click', '.btn-remove', function () {
+        $(this).parents('.search-item').remove();
+      });
+  }); 
+
+  $('.btn-del').click(function(){
+     $(this).parents('.loc-item').fadeOut( "slow", function() {
+         $(this).parents('.loc-item').remove();
+     });
+  });
 
 });
