1c7b377508e5374eec9d7398b1e816603103e9bd
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2014-12-15 11:57:55 +0530

    added company logo

--- app/helpers/freights_helper.rb
+++ app/helpers/freights_helper.rb
@@ -1,9 +1,11 @@
 module FreightsHelper
 
-def delivery_date(freight_rate)
-  freight_rate.cut_off_date + freight_rate.transition_days.to_i.days 
+  def delivery_date(freight_rate)
+    freight_rate.cut_off_date + freight_rate.transition_days.to_i.days 
+  end
 
-
-end
+  def company_logo(freight_rate)
+    freight_rate.seller.avatar.url
+  end
 
 end
