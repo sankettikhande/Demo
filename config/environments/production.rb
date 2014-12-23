c6016d2dcbe0348e985278e940f36f4c16b748e2
KiranKumara <kiran.kumara@sodelsolutions.com>
2014-12-23 16:26:52 +0530

    added mailer configuration in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -75,4 +75,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'kiran.test.mail1@gmail.com',
+    password:             'testmail',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
+  config.action_mailer.default_url_options = { :host => 'bluecompass.sodelsolutions.com' }
+
 end
