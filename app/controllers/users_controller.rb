8d80cbc4834575826bc4dd0c591ced8a43f963ff
sankettikhande <sanket.tikhande@sidusa.com>
2015-02-04 19:25:11 +0530

    changes for update user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,28 @@ class UsersController < ApplicationController
 
   end
 
+  def update_user_information
+    user = current_user if current_user.seller?
+      if params[:user_information].present?
+        user.update_attributes!(:first_name => params[:first_name],:last_name=>params[:last_name], :company_name=>params[:comp_name], :company_registration_number=>params[:comp_reg],:address_line_1=>params[:add1],:address_line_2=> params[:add2],:address_line_3=>params[:add3],:post_code=>params[:post_code],:preferred_currency=>params[:currency])
+      else
+        user.update_attribute!(:email=> params[:email])
+      end
+    user.save
+    respond_to do |format| 
+      format.js
+    end
+  end
+
+  def destroy_user
+    user = current_user if current_user.seller?    
+    user.destroy if user
+    respond_to do |format|
+      format.js
+    end
+
+  end
+
   def register_supplier_consignee
     address_books = current_user.address_books
     @suppliers = address_books.supplier_address
