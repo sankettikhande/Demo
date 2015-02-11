6ab9e6797d85de8f4c025dd20dc838b6fe66fde0
mauryaranjeet <ranjeet.maurya@sodelsolutions.com>
2015-02-11 16:35:06 +0530

    fixed the error for guest user sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def register_guest_user
-
     if guest_user_params.values.any?{|v| v.blank?}
-      render :template => "freights/guest_user_sign_up", locals: { notice: "All fields are required" }
-    else
-      email = params[:user][:email]
-      return render :template => "freights/guest_user_sign_up", locals: { notice: "Email Allready Exist Please login !" } if User.exists?(email: email)
+      respond_to do |format|
+        format.js { render :js => "$('.signup_form').html('All fields are required').show();" }
+      end
+    elsif User.exists?(email: params[:user][:email])
+      respond_to do |format|
+        format.js { render :js => "$('.signup_form').html('Email Allready Exist Please login !').show();" }
+      end      
+    else  
+      email = params[:user][:email]      
       user = User.new(guest_user_params.merge({role: :guest, password: email, password_confirmation: email}))
       user.skip_confirmation!
       create_user = user.save(validate: false)
@@ -14,11 +18,11 @@ class RegistrationsController < Devise::RegistrationsController
       if create_user
         flash[:notice] = "Account successfully created passward sent to your email"
         UserMailer.send_password_to_guest_user(email).deliver
-        redirect_to session[:user_return_to]
+        respond_to do |format|
+          format.js { render :js => "window.location.href='#{session[:user_return_to]}'" }
+        end
       end
-      #redirect_to search_freights_path
     end
-
   end
 
   private
